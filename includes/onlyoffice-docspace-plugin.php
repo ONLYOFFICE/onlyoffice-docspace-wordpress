3bed67b9e98f920a25d84544380222a5aa577ecb
Andrei Panov <andrew.panov@onlyoffice.com>
2022-11-14 09:47:44 +0000

    plugin loader

--- includes/onlyoffice-docspace-plugin.php
+++ includes/onlyoffice-docspace-plugin.php
@@ -3,6 +3,7 @@
 class OODSPlugin
 {
 
+    protected $loader;
     protected $plugin_name;
     protected $version;
     public function __construct()
@@ -11,8 +12,18 @@ class OODSPlugin
         $this->version = ONLYOFFICE_DOCSPACE_PLUGIN_VERSION;
         $this->plugin_name = 'onlyoffice-docspace-plugin';
 
+        $this->load_dependencies();
+    }
+
+    private function load_dependencies()
+    {
+
+        require_once plugin_dir_path(dirname(__FILE__)) . 'includes/onlyoffice-docspace-loader.php';
+        $this->loader = new OODSP_Loader();
     }
 
     public function run()
     {
+        $this->loader->run();
+    }
     }
