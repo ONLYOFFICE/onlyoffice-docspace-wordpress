2b21f7c42040f51abb0540d90b021094a84e368c
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2023-04-13 12:21:52 +0300

    added controls

--- onlyoffice-docspace-wordpress-block/build/index.asset.php
+++ onlyoffice-docspace-wordpress-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => 'b99f3bf1ae673e74a2e4');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '0e8a571eda69d5d10899');
--- onlyoffice-docspace-wordpress-block/src/edit.js
+++ onlyoffice-docspace-wordpress-block/src/edit.js
@@ -16,8 +16,9 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-import { useBlockProps } from '@wordpress/block-editor';
-import { Button, Placeholder, Modal } from '@wordpress/components';
+import { useBlockProps, InspectorControls } from '@wordpress/block-editor';
+import { CheckboxControl, Button, Placeholder, Modal,    PanelBody,
+    __experimentalInputControl as InputControl } from '@wordpress/components';
 import { useState } from '@wordpress/element';
 import { blockStyle, onlyofficeIcon } from "./index";
 
@@ -25,26 +26,17 @@ const Edit = ({ attributes, setAttributes }) => {
 
     const blockProps = useBlockProps({ style: blockStyle });
 
-    if (!attributes.frameId) {
-        setAttributes({ frameId: DocSpaceComponent.generateId() });
-    }
+    // if (!attributes.frameId) {
+    //     setAttributes({ frameId: DocSpaceComponent.generateId() });
+    // }
 
-    if (!attributes.frameConfig && attributes.frameId) {
-        setAttributes({
-            frameConfig: {
-                "height": "400px",
-                "frameId": "ds-frame-" + attributes.frameId,
-                "mode": "manager",
-                "showHeader": false,
-                "showTitle": true,
-                "showMenu": false,
-                "showFilter": false,
-                "showAction": false,
-            }
-        });
-    }
+    // if (!attributes.frameConfig && attributes.frameId) {
+    //     setAttributes({
+
 
     var init = false;
+
+    DocSpaceComponent.initScript();
 
     const [isOpen, setOpen] = useState( false );
     const [title, setTitle] = useState( "" );
@@ -61,12 +53,27 @@ const Edit = ({ attributes, setAttributes }) => {
                 setTitle( "Select file" );
         }
 
-        setOpen( true );
+         setOpen( true );
 
-        DocSpaceComponent.initScript().then(function () {
-            if (!window.DocSpace || init) return;
-            init = true;
-            DocSpace.initFrame(attributes.frameConfig)
+        var frameConfig = {
+            "width": attributes.width || "800px",
+            "frameId": "ds-frame-select",
+            "height": attributes.height || "800px",
+            "mode": attributes.mode || "manager",
+            "showHeader": attributes.showHeader,
+            "showTitle": attributes.showTitle,
+            "showMenu": attributes.showMenu,
+            "showFilter": attributes.showFilter,
+            "showAction": attributes.showAction
+        };
+
+        if (attributes.src)  frameConfig.src =attributes.src ;
+        if (attributes.rootPath)  frameConfig.rootPath =attributes.rootPath ;
+
+        console.log(frameConfig);
+        DocSpaceComponent.initScript().then(function(){
+            DocSpace.initFrame(frameConfig);
+
         });
     }
     const closeModal = (e) => {
@@ -76,6 +83,20 @@ const Edit = ({ attributes, setAttributes }) => {
     }
     return (
         <div {...blockProps}>
+            <InspectorControls key="setting">
+                    <PanelBody title={ 'Settings' }>
+                        <InputControl label={' Width' } value={attributes.width} onChange={ ( value ) => setAttributes({ width: value }) } />
+                        <InputControl label={ 'Height' } value={attributes.height} onChange={ ( value ) => setAttributes({ height: value }) } />
+                        <InputControl label={ 'src' } value={attributes.src} onChange={ ( value ) => setAttributes({ src: value }) } />
+                        <InputControl label={ 'rootPath' } value={attributes.rootPath} onChange={ ( value ) => setAttributes({ rootPath: value }) } />
+                        <InputControl label={ 'mode' } value={attributes.mode} onChange={ ( value ) => setAttributes({ mode: value }) } />
+                        <CheckboxControl label={ 'showHeader' } checked={attributes.showHeader} onChange={ ( value ) => setAttributes({ showHeader: value }) } />
+                        <CheckboxControl label={ 'showTitle' } checked={attributes.showTitle} onChange={ ( value ) => setAttributes({ showTitle: value }) } />
+                        <CheckboxControl label={ 'showMenu' } checked={attributes.showMenu} onChange={ ( value ) => setAttributes({ showMenu: value }) } />
+                        <CheckboxControl label={ 'showFilter' } checked={attributes.showFilter} onChange={ ( value ) => setAttributes({ showFilter: value }) } />
+                        <CheckboxControl label={ 'showAction' } checked={attributes.showAction} onChange={ ( value ) => setAttributes({ showAction: value }) } />
+                    </PanelBody>
+            </InspectorControls>
             <Placeholder
                 icon={onlyofficeIcon} 
                 label="ONLYOFFICE DocSpace"
@@ -98,7 +119,7 @@ const Edit = ({ attributes, setAttributes }) => {
             </Placeholder>
             { isOpen && (
                 <Modal onRequestClose={ closeModal } title={ title }>
-                    <div id={"ds-frame-" + attributes.frameId}>Fallback text</div>
+                    <div id="ds-frame-select">Fallback text</div>
                 </Modal>
             ) }
         </div>
