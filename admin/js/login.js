2cc6e2d63f1b0c921e8e43c6725843bd584df140
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2023-05-25 13:09:07 +0300

    refactoring: wpcs admin/css and admin/js

--- admin/js/login.js
+++ admin/js/login.js
@@ -1,54 +1,67 @@
 /**
- * @output includes/js/oodsp-login.js
+ * JS for login in DocSpace.
+ *
+ * @package Onlyoffice_Docspace_Wordpress
  */
 
 window.wp = window.wp || {};
 
 ( function( $, wp ) {
-    wp.oodsp = wp.oodsp || {};
+	wp.oodsp = wp.oodsp || {};
 
-    var __ = wp.i18n.__;
+	var __ = wp.i18n.__;
 
-    wp.oodsp.login = function (frameId, url, email, error, callback) {
-        const loginTemplate = wp.template( 'oodsp-login' );
+	wp.oodsp.login = function (frameId, url, email, error, callback) {
+		const loginTemplate = wp.template( 'oodsp-login' );
 
-        $('#' + frameId).html(loginTemplate({ 
-            email: email, 
-            domain: new URL(url).host,
-            error: error ? messages['auth-failed'] : null
-        }));
+		$( '#' + frameId ).html(
+			loginTemplate(
+				{
+					email: email,
+					domain: new URL( url ).host,
+					error: error ? messages['auth-failed'] : null
+				}
+			)
+		);
 
-        $('#oodsp-login-form').submit( function( event ) {
-            window.DocSpaceComponent.onAppReady = false; //ToDo: remove
-            event.preventDefault();
-            $("#login_error").html();
+		$( '#oodsp-login-form' ).submit(
+			function( event ) {
+				window.DocSpaceComponent.onAppReady = false; // ToDo: remove.
+				event.preventDefault();
+				$( '#login_error' ).html();
 
-            var password = $('#oodsp-password').val();
+				var password = $( '#oodsp-password' ).val();
 
-            if (password.trim() == "" ) {
-                $("#login_error").html(messages['empty-password']);
-                $("#login_error").show();
-            } else {
-                callback( password );
-            }
-        });
-
-        $('.wp-hide-pw').on( 'click', function () {
-            if ( 'password' === $('#oodsp-password').attr( 'type' ) ) {
-				$('#oodsp-password').attr( 'type', 'text' );
-				resetToggle( false );
-			} else {
-				$('#oodsp-password').attr( 'type', 'password' );
-				resetToggle( true );
+				if ( '' == password.trim() ) {
+					$( '#login_error' ).html( messages['empty-password'] );
+					$( '#login_error' ).show();
+				} else {
+					callback( password );
+				}
 			}
-        });
-    };
+		);
 
-    function resetToggle( show ) {
-		$('.wp-hide-pw')
-			.attr({
-				'aria-label': show ? __( 'Show password' ) : __( 'Hide password' )
-			})
+		$( '.wp-hide-pw' ).on(
+			'click',
+			function () {
+				if ( 'password' === $( '#oodsp-password' ).attr( 'type' ) ) {
+					$( '#oodsp-password' ).attr( 'type', 'text' );
+					resetToggle( false );
+				} else {
+					$( '#oodsp-password' ).attr( 'type', 'password' );
+					resetToggle( true );
+				}
+			}
+		);
+	};
+
+	function resetToggle( show ) {
+		$( '.wp-hide-pw' )
+			.attr(
+				{
+					'aria-label': show ? __( 'Show password' ) : __( 'Hide password' )
+				}
+			)
 			.find( '.text' )
 				.text( show ? __( 'Show' ) : __( 'Hide' ) )
 			.end()
@@ -56,5 +69,4 @@ window.wp = window.wp || {};
 				.removeClass( show ? 'dashicons-hidden' : 'dashicons-visibility' )
 				.addClass( show ? 'dashicons-visibility' : 'dashicons-hidden' );
 	}
-
 }( window.jQuery, window.wp ));
--- admin/js/settings.js
+++ admin/js/settings.js
@@ -1,28 +1,33 @@
+/**
+ * JS for OODSP_Settings.
+ *
+ * @package Onlyoffice_Docspace_Wordpress
+ */
 
-( function( $ ) {
-    const validateSettings = function () {
-        const controls = [
-            $('#docspace_url'),
-            $('#docspace_login'),
-            $('#user_pass')
-        ]
+(function( $ ) {
+	const validateSettings = function () {
+		const controls = [
+			$( '#docspace_url' ),
+			$( '#docspace_login' ),
+			$( '#user_pass' )
+		]
 
-        var result = true;
+		var result = true;
 
-        for (var control of controls) {
-            const value = control.val() || '';
-            if (value.trim() === '') {
-                control.parents('.form-field').addClass('form-invalid');
-                result = false;
-            } else {
-                control.parents('.form-field').removeClass('form-invalid');
-            }
-        }
+		for ( var control of controls ) {
+			const value = control.val() || '';
+			if ( '' === value.trim() ) {
+				control.parents( '.form-field' ).addClass( 'form-invalid' );
+				result = false;
+			} else {
+				control.parents( '.form-field' ).removeClass( 'form-invalid' );
+			}
+		}
 
-        return result;
-    };
+		return result;
+	};
 
-    const addNotice = function ( message, type ) {
+	const addNotice = function ( message, type ) {
 		var $notice = $( '<div></div>' )
 			.attr( 'role', 'alert' )
 			.attr( 'tabindex', '-1' )
@@ -35,139 +40,168 @@
 					.append( $( '<span></span>' ).addClass( 'screen-reader-text' ).text( wp.i18n.__( 'Dismiss this notice.' ) ) )
 			);
 
-		$('#onlyoffice-docspace-settings-notice').append( $notice );
+		$( '#onlyoffice-docspace-settings-notice' ).append( $notice );
 
 		return $notice;
 	};
 
 	const clearNotices = function() {
-		$( '.notice', $('#wpbody-content') ).remove();
+		$( '.notice', $( '#wpbody-content' ) ).remove();
 	};
 
-    const showLoader = function() {
-		$('#onlyoffice-docspace-settings-loader').show();
+	const showLoader = function() {
+		$( '#onlyoffice-docspace-settings-loader' ).show();
 	};
 
-    const hideLoader = function() {
-		$('#onlyoffice-docspace-settings-loader').hide();
+	const hideLoader = function() {
+		$( '#onlyoffice-docspace-settings-loader' ).hide();
 	};
 
-    const generatePass = function() {
-        var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
-        var passwordLength = 24;
-        var password = "";
+	const generatePass = function() {
+		var chars          = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
+		var passwordLength = 24;
+		var password       = "";
 
-        for (var i = 0; i <= passwordLength; i++) {
-            var randomNumber = Math.floor(Math.random() * chars.length);
-            password += chars.substring(randomNumber, randomNumber +1);
-        }
+		for (var i = 0; i <= passwordLength; i++) {
+			var randomNumber = Math.floor( Math.random() * chars.length );
+			password        += chars.substring( randomNumber, randomNumber + 1 );
+		}
 
-        return password;
-    }
+		return password;
+	}
 
-    const settingsForm = $('#onlyoffice-docspace-settings');
+	const settingsForm = $( '#onlyoffice-docspace-settings' );
 
-    settingsForm.on('submit', function () {
-        const hash = $('#hash');
-        
-        if (!hash.length) {
-            clearNotices();
-            showLoader();
+	settingsForm.on(
+		'submit',
+		function () {
+			const hash = $( '#hash' );
 
-            if (!validateSettings()) {
-                hideLoader();
-                return false;
-            }
+			if ( ! hash.length ) {
+				clearNotices();
+				showLoader();
 
-            const pass = $('#user_pass').val().trim();
-            DocSpaceComponent.initScript($('#docspace_url').val().trim())
-                .then(async function() {
-                    DocSpace.SDK.initSystem({
-                        frameId: "oodsp-system-frame",
-                        events: {
-                            "onAppReady": async function() {
-                                if (!window.DocSpaceComponent.onAppReady) { // ToDo: Delete after fixes
-                                    window.DocSpaceComponent.onAppReady = true;
-                                    const hashSettings = await DocSpace.SDK.frames["oodsp-system-frame"].getHashSettings();
-                                    const hash = await DocSpace.SDK.frames["oodsp-system-frame"].createHash(pass.trim(), hashSettings);
-                                    settingsForm.append(
-                                        $( '<input />' )
-                                            .attr('id', "hash")
-                                            .attr('name', "docspace_pass")
-                                            .attr('hidden', "true")
-                                            .attr('value', hash));
+				if ( ! validateSettings() ) {
+					hideLoader();
+					return false;
+				}
 
-                                    const hashCurrentUser = await DocSpace.SDK.frames["oodsp-system-frame"].createHash(generatePass(), hashSettings);
-                                    settingsForm.append(
-                                        $( '<input />' )
-                                            .attr('id', "hash-current-user")
-                                            .attr('name', "hash_current_user")
-                                            .attr('hidden', "true")
-                                            .attr('value', hashCurrentUser));
+				const pass = $( '#user_pass' ).val().trim();
+				DocSpaceComponent.initScript( $( '#docspace_url' ).val().trim() )
+					.then(
+						async function() {
+							DocSpace.SDK.initSystem(
+								{
+									frameId: "oodsp-system-frame",
+									events: {
+										"onAppReady": async function() {
+											if ( ! window.DocSpaceComponent.onAppReady ) { // ToDo: Delete after fixes.
+												window.DocSpaceComponent.onAppReady = true;
 
-                                    $('#user_pass').val('')
-                                    settingsForm.submit();
-                                }
-                            },
-                            "onAppError": function() {
-                                alert("onAppError");
-                            }
-                        }
-                    });
-                }).catch(function() {
-                    const errorMessage = "Undefined API"; //ToDo: message i18n
-                    hideLoader();
-                    addNotice(errorMessage, "error");
-                });
-            
-            return false;
-        } else {
-            return true;
-        } 
-    } );
+												const hashSettings = await DocSpace.SDK.frames['oodsp-system-frame'].getHashSettings();
+												const hash         = await DocSpace.SDK.frames['oodsp-system-frame'].createHash( pass.trim(), hashSettings );
+												settingsForm.append(
+													$( '<input />' )
+														.attr( 'id', 'hash' )
+														.attr( 'name', 'docspace_pass' )
+														.attr( 'hidden', 'true' )
+														.attr( 'value', hash )
+												);
 
-    const usersForm = $('#onlyoffice-docspace-settings-users');
+												const hashCurrentUser = await DocSpace.SDK.frames['oodsp-system-frame'].createHash( generatePass(), hashSettings );
+												settingsForm.append(
+													$( '<input />' )
+														.attr( 'id', 'hash-current-user' )
+														.attr( 'name', 'hash_current_user' )
+														.attr( 'hidden', 'true' )
+														.attr( 'value', hashCurrentUser )
+												);
 
-    usersForm.on('submit', async function (event) {
-        if (event.originalEvent.submitter.id === 'doaction' && !usersForm.attr("hashGenerated")) {
-            event.preventDefault();
-            showLoader();
+												$( '#user_pass' ).val( '' )
+												settingsForm.submit();
+											}
+										},
+										'onAppError': function() {
+											alert( 'onAppError' );
+										}
+									}
+								}
+							);
+						}
+					).catch(
+						function() {
+							const errorMessage = 'Undefined API'; // ToDo: message i18n.
+							hideLoader();
+							addNotice( errorMessage, 'error' );
+						}
+					);
+				return false;
+			} else {
+				return true;
+			}
+		}
+	);
 
-            const hashSettings = await DocSpace.SDK.frames['oodsp-system-frame'].getHashSettings();
+	const usersForm = $( '#onlyoffice-docspace-settings-users' );
 
-            const users = $('th.check-column[scope="row"] input');
+	usersForm.on(
+		'submit',
+		async function (event) {
+			if ('doaction' === event.originalEvent.submitter.id && ! usersForm.attr( 'hashGenerated' ) ) {
+				event.preventDefault();
+				showLoader();
 
-            for (var user of users) {
-                if ($(user).is(':checked')) {
-                    const hash = await DocSpace.SDK.frames['oodsp-system-frame'].createHash(generatePass(), hashSettings);
+				const hashSettings = await DocSpace.SDK.frames['oodsp-system-frame'].getHashSettings();
 
-                    $(user).val($(user).val() + "$$" + hash);
-                }
-            }
+				const users = $( 'th.check-column[scope="row"] input' );
 
-            usersForm.attr('hashGenerated', true);
-            usersForm.attr('action', 'admin.php?page=onlyoffice-docspace-settings&users=true');
-            usersForm.attr('method', 'POST');
-            usersForm.submit();
-        }
-    } );
+				for (var user of users) {
+					if ( $( user ).is( ':checked' ) ) {
+						const hash = await DocSpace.SDK.frames['oodsp-system-frame'].createHash( generatePass(), hashSettings );
 
-    $('#wpbody-content').on( 'click', '.notice-dismiss', function( e ) {
-		e.preventDefault();
-		var $el = $( this ).parent();
-		$el.removeAttr( 'role' );
-		$el.fadeTo( 100, 0, function () {
-			$el.slideUp( 100, function () {
-				$el.remove();
-				$('#wpbody-content').trigger( 'focus' );
-			} );
-		} );
-	} );
+						$( user ).val( $( user ).val() + "$$" + hash );
+					}
+				}
 
-    DocSpaceComponent.initScript()
-        .then(function(e) { // ToDo: onAppReady, onError
-            DocSpace.SDK.initSystem({
-                frameId: "oodsp-system-frame"
-            });
-        });
+				usersForm.attr( 'hashGenerated', true );
+				usersForm.attr( 'action', 'admin.php?page=onlyoffice-docspace-settings&users=true' );
+				usersForm.attr( 'method', 'POST' );
+				usersForm.submit();
+			}
+		}
+	);
+
+	$( '#wpbody-content' ).on(
+		'click',
+		'.notice-dismiss',
+		function( e ) {
+			e.preventDefault();
+			var $el = $( this ).parent();
+			$el.removeAttr( 'role' );
+			$el.fadeTo(
+				100,
+				0,
+				function () {
+					$el.slideUp(
+						100,
+						function () {
+							$el.remove();
+							$( '#wpbody-content' ).trigger( 'focus' );
+						}
+					);
+				}
+			);
+		}
+	);
+
+	DocSpaceComponent.initScript()
+		.then(
+			function(e) { // ToDo: onAppReady, onError.
+				DocSpace.SDK.initSystem(
+					{
+						frameId: 'oodsp-system-frame'
+					}
+				);
+			}
+		);
 }( jQuery ) );
