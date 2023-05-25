b9922a3b6c886a7780c0e89945aa4f00486d91ff
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2023-05-25 12:34:55 +0300

    refactoring: wpcs settings/actions

--- admin/settings/actions/settings-invite-users.php
+++ admin/settings/actions/settings-invite-users.php
@@ -2,106 +2,148 @@
 /**
  * Invite ONLYOFFICE DocSpace users action.
  *
- * @package Onlyoffice_Docspace_Plugin
- * @subpackage Onlyoffice_Docspace_Plugin/includes/settings/actions
+ * @package Onlyoffice_Docspace_Wordpress
+ * @subpackage Onlyoffice_Docspace_Wordpress/admin/settings/actions
  */
 
+/**
+ * Innite users.
+ */
 function invite_users() {
-    if ( isset( $_REQUEST['wp_http_referer'] ) ) {
-        $redirect = remove_query_arg( array( 'wp_http_referer', 'updated', 'delete_count' ), wp_unslash( $_REQUEST['wp_http_referer'] ) );
-    } else {
-        $redirect = 'admin.php?page=onlyoffice-docspace-settings&users=true';
-    }
+	if ( isset( $_REQUEST['wp_http_referer'] ) ) {
+		$redirect = remove_query_arg( array( 'wp_http_referer', 'updated', 'delete_count' ), sanitize_text_field( wp_unslash( $_REQUEST['wp_http_referer'] ) ) );
+	} else {
+		$redirect = 'admin.php?page=onlyoffice-docspace-settings&users=true';
+	}
 
-    check_admin_referer( 'bulk-users' );
+	check_admin_referer( 'bulk-users' );
 
-    if ( empty( $_REQUEST['users'] ) ) {
-        wp_redirect( $redirect );
-        exit;
-    }
+	if ( empty( $_REQUEST['users'] ) ) {
+		wp_safe_redirect( $redirect );
+		exit;
+	}
 
-    if (is_array($_REQUEST['users'])) {
-        $users = array_map( function(string $user) {
-            $user = explode('$$', $user, 2);
-            return array(
-                'id' => $user[0],
-                'hash' => $user[1] 
-            );
-        }, (array) $_REQUEST['users'] );
-    }
+	if ( is_array( $_REQUEST['users'] ) ) {
+		$users = array_map(
+			function( string $user ) {
+				$user = explode( '$$', $user, 2 );
+				return array(
+					'id'   => $user[0],
+					'hash' => $user[1],
+				);
+			},
+			(array) wp_unslash( $_REQUEST['users'] ) // phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
+		);
+	}
 
-    if ( empty( $users ) ) {
-        wp_redirect( $redirect );
-        exit;
-    }
+	if ( empty( $users ) ) {
+		wp_safe_redirect( $redirect );
+		exit;
+	}
 
-    $oodsp_request_manager = new OODSP_Request_Manager();
-    $res_docspace_users = $oodsp_request_manager->request_docspace_users();
+	$oodsp_request_manager = new OODSP_Request_Manager();
+	$res_docspace_users    = $oodsp_request_manager->request_docspace_users();
 
-    if ( $res_docspace_users['error'] ) {
-        add_oodsp_users_message( 'users_invited', __( 'Error getting users from ONLYOFFICE DocSpace', 'onlyoffice-docspace-plugin' ), 'error' );
-        set_transient( 'oodsp_users_messages', get_oodsp_users_messages(), 30 );
+	if ( $res_docspace_users['error'] ) {
+		add_oodsp_users_message( 'users_invited', __( 'Error getting users from ONLYOFFICE DocSpace', 'onlyoffice-docspace-plugin' ), 'error' );
+		set_transient( 'oodsp_users_messages', get_oodsp_users_messages(), 30 );
 
-        wp_safe_redirect( admin_url( 'admin.php?page=onlyoffice-docspace-settings&users=true&invited=true' ) );
-        exit;
-    }
+		wp_safe_redirect( admin_url( 'admin.php?page=onlyoffice-docspace-settings&users=true&invited=true' ) );
+		exit;
+	}
 
-    $docspace_users = array_map( 
-        function($docspace_user) {
-            return $docspace_user['email'];
-        },
-        $res_docspace_users['data']
-    );
+	$docspace_users = array_map(
+		function( $docspace_user ) {
+			return $docspace_user['email'];
+		},
+		$res_docspace_users['data']
+	);
 
-    $count_invited = $count_skipped = $count_error = 0;
+	$count_invited = 0;
+	$count_skipped = 0;
+	$count_error   = 0;
 
-    foreach ( $users as $user ) {
-        $user_id   = $user['id'];
-        $user_hash = $user['hash'];
+	foreach ( $users as $user ) {
+		$user_id   = $user['id'];
+		$user_hash = $user['hash'];
 
-        $user = get_user_to_edit( $user_id  );
+		$user = get_user_to_edit( $user_id );
 
-        if ( in_array( $user->user_email, $docspace_users ) ) {
-            $count_skipped++;
-        } else {
-            $res_invite_user = $oodsp_request_manager->request_invite_user(
-                $user->user_email,
-                $user_hash,
-                $user->first_name,
-                $user->last_name,
-                2
-            );
+		if ( in_array( $user->user_email, $docspace_users, true ) ) {
+			$count_skipped++;
+		} else {
+			$res_invite_user = $oodsp_request_manager->request_invite_user(
+				$user->user_email,
+				$user_hash,
+				$user->first_name,
+				$user->last_name,
+				2
+			);
 
-            if ( $res_invite_user['error'] ){
-                $count_error++;
-            } else {
-                $oodsp_security_manager = new OODSP_Security_Manager();
+			if ( $res_invite_user['error'] ) {
+				$count_error++;
+			} else {
+				$oodsp_security_manager = new OODSP_Security_Manager();
 
 				$oodsp_security_manager->set_oodsp_user_pass( $user_id, $user_hash );
 
-                $count_invited++;
-            } 
-        }
-    }
-    
-    if ( $count_error !== 0 ) {
-        add_oodsp_users_message( 'users_invited', sprintf( __( 'Invite with error for %s/%s users', 'onlyoffice-docspace-plugin' ),  $count_error,  count( $users ) ), 'error' );
-    }
-    
-    if ( $count_skipped !== 0 ) {
-        add_oodsp_users_message( 'users_invited', sprintf( __('Invite skipped for %s/%s users', 'onlyoffice-docspace-plugin'),  $count_skipped,  count( $users ) ), 'warning' );
-    }
+				$count_invited++;
+			}
+		}
+	}
 
-    if ( $count_invited !== 0 ) {
-        add_oodsp_users_message( 'users_invited', sprintf( __( 'Invite sucessed for %s/%s users', 'onlyoffice-docspace-plugin' ),  $count_invited,  count( $users ) ), 'success' );
-    }
+	if ( 0 !== $count_error ) {
+		add_oodsp_users_message(
+			'users_invited',
+			sprintf(
+				/* translators: %1$s: count error; %2$s: count users */
+				__( 'Invite with error for %1$s/%2$s users', 'onlyoffice-docspace-plugin' ),
+				$count_error,
+				count( $users )
+			),
+			'error'
+		);
+	}
 
-    set_transient( 'oodsp_users_messages', get_oodsp_users_messages(), 30 );
+	if ( 0 !== $count_skipped ) {
+		add_oodsp_users_message(
+			'users_invited',
+			sprintf(
+				/* translators: %1$s: count skiped; %2$s: count users */
+				__( 'Invite skipped for %1$s/%2$s users', 'onlyoffice-docspace-plugin' ),
+				$count_skipped,
+				count( $users )
+			),
+			'warning'
+		);
+	}
 
-    wp_safe_redirect( admin_url( 'admin.php?page=onlyoffice-docspace-settings&users=true&invited=true' ) );
-    exit;
+	if ( 0 !== $count_invited ) {
+		add_oodsp_users_message(
+			'users_invited',
+			sprintf(
+				/* translators: %1$s: count invited; %2$s: count users */
+				__( 'Invite sucessed for %1$s/%2$s users', 'onlyoffice-docspace-plugin' ),
+				$count_invited,
+				count( $users )
+			),
+			'success'
+		);
+	}
+
+	set_transient( 'oodsp_users_messages', get_oodsp_users_messages(), 30 );
+
+	wp_safe_redirect( admin_url( 'admin.php?page=onlyoffice-docspace-settings&users=true&invited=true' ) );
+	exit;
 }
 
+/**
+ * Add oodsp users messages.
+ *
+ * @param string $code The code.
+ * @param string $message The message.
+ * @param string $type The type.
+ */
 function add_oodsp_users_message( $code, $message, $type = 'error' ) {
 	global $wp_oodsp_users_messages;
 
@@ -112,10 +154,13 @@ function add_oodsp_users_message( $code, $message, $type = 'error' ) {
 	);
 }
 
+/**
+ * Return oodsp users messages.
+ */
 function get_oodsp_users_messages() {
 	global $wp_oodsp_users_messages;
 
-	if ( isset( $_GET['users'] ) && $_GET['users'] && get_transient( 'oodsp_users_messages' ) ) {
+	if ( isset( $_GET['users'] ) && wp_unslash( $_GET['users'] ) && get_transient( 'oodsp_users_messages' ) ) { // phpcs:ignore WordPress.Security.NonceVerification, WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
 		$wp_oodsp_users_messages = array_merge( (array) $wp_oodsp_users_messages, get_transient( 'oodsp_users_messages' ) );
 		delete_transient( 'oodsp_users_messages' );
 	}
@@ -127,14 +172,15 @@ function get_oodsp_users_messages() {
 	return $wp_oodsp_users_messages;
 }
 
+/**
+ * Return oodsp users messages.
+ */
 function oodsp_users_messages() {
 	$oodsp_users_messages = get_oodsp_users_messages();
 
 	if ( empty( $oodsp_users_messages ) ) {
 		return;
 	}
-
-	$output = '';
 
 	foreach ( $oodsp_users_messages as $key => $details ) {
 		if ( 'updated' === $details['type'] ) {
@@ -154,10 +200,8 @@ function oodsp_users_messages() {
 			esc_attr( $details['type'] )
 		);
 
-		$output .= "<div id='$css_id' class='$css_class'> \n";
-		$output .= "<p><strong>{$details['message']}</strong></p>";
-		$output .= "</div> \n";
+		echo '<div id="' . esc_attr( $css_id ) . '" class="' . esc_attr( $css_class ) . '">';
+		echo '<p><strong>' . esc_html( $details['message'] ) . '</strong></p>';
+		echo '</div>';
 	}
-
-	echo $output;
 }
--- admin/settings/actions/settings-update.php
+++ admin/settings/actions/settings-update.php
@@ -2,104 +2,138 @@
 /**
  * Update ONLYOFFICE DocSpace Setting action.
  *
- * @package Onlyoffice_Docspace_Plugin
- * @subpackage Onlyoffice_Docspace_Plugin/includes/settings/actions
+ * @package Onlyoffice_Docspace_Wordpress
+ * @subpackage Onlyoffice_Docspace_Wordpress/admin/settings/actions
  */
 
+/**
+ * Update settings.
+ */
 function update_settings() {
-    if ( isset( $_POST[OODSP_Settings::DOCSPACE_URL] ) 
-            && isset( $_POST[OODSP_Settings::DOCSPACE_LOGIN] ) 
-            && isset( $_POST[OODSP_Settings::DOCSPACE_PASS] )
-            && isset( $_POST['hash_current_user'] )
-            ) {
-        check_admin_referer( 'onlyoffice_docspace_settings-options' );
+	if ( isset( $_POST[ OODSP_Settings::DOCSPACE_URL ] )
+			&& isset( $_POST[ OODSP_Settings::DOCSPACE_LOGIN ] )
+			&& isset( $_POST[ OODSP_Settings::DOCSPACE_PASS ] )
+			&& isset( $_POST['hash_current_user'] )
+			) {
+		check_admin_referer( 'onlyoffice_docspace_settings-options' );
 
-        $docspace_url = prepare_value( $_POST[OODSP_Settings::DOCSPACE_URL] );
-        $docspace_login = prepare_value( $_POST[OODSP_Settings::DOCSPACE_LOGIN] );
-        $docspace_pass = prepare_value( $_POST[OODSP_Settings::DOCSPACE_PASS] );
-        $hash_current_user = prepare_value( $_POST['hash_current_user'] );
+		$docspace_url      = prepare_value( sanitize_text_field( wp_unslash( $_POST[ OODSP_Settings::DOCSPACE_URL ] ) ) );
+		$docspace_login    = prepare_value( sanitize_text_field( wp_unslash( $_POST[ OODSP_Settings::DOCSPACE_LOGIN ] ) ) );
+		$docspace_pass     = prepare_value( sanitize_text_field( wp_unslash( $_POST[ OODSP_Settings::DOCSPACE_PASS ] ) ) );
+		$hash_current_user = prepare_value( sanitize_text_field( wp_unslash( $_POST['hash_current_user'] ) ) );
 
-        $docspace_url = substr($docspace_url, -1) === '/' ? $docspace_url : $docspace_url . '/';
+		$docspace_url = '/' === substr( $docspace_url, -1 ) ? $docspace_url : $docspace_url . '/';
 
-        $oodsp_request_manager = new OODSP_Request_Manager();
+		$oodsp_request_manager = new OODSP_Request_Manager();
 
-        $res_auth = $oodsp_request_manager->auth_docspace( $docspace_url, $docspace_login, $docspace_pass );
+		$res_auth = $oodsp_request_manager->auth_docspace( $docspace_url, $docspace_login, $docspace_pass );
 
-        if ( $res_auth['error'] === OODSP_Request_Manager::UNAUTHORIZED) {
-            add_settings_error( 'general', 'settings_updated', __( 'Invalid credentials. Please try again!', 'onlyoffice-docspace-plugin' ) );
-        }
-        if ( $res_auth['error'] === OODSP_Request_Manager::USER_NOT_FOUND) {
-            add_settings_error( 'general', 'settings_updated', __( 'Error getting data user. Please try again!', 'onlyoffice-docspace-plugin' ) );
-        }
-        if ( $res_auth['error'] === OODSP_Request_Manager::FORBIDDEN) {
-            add_settings_error( 'general', 'settings_updated', __( 'The specified user is not a DocSpace administrator!', 'onlyoffice-docspace-plugin' ) );
-        }
+		if ( OODSP_Request_Manager::UNAUTHORIZED === $res_auth['error'] ) {
+			add_settings_error( 'general', 'settings_updated', __( 'Invalid credentials. Please try again!', 'onlyoffice-docspace-plugin' ) );
+		}
+		if ( OODSP_Request_Manager::USER_NOT_FOUND === $res_auth['error'] ) {
+			add_settings_error( 'general', 'settings_updated', __( 'Error getting data user. Please try again!', 'onlyoffice-docspace-plugin' ) );
+		}
+		if ( OODSP_Request_Manager::FORBIDDEN === $res_auth['error'] ) {
+			add_settings_error( 'general', 'settings_updated', __( 'The specified user is not a DocSpace administrator!', 'onlyoffice-docspace-plugin' ) );
+		}
 
-        if ( ! get_settings_errors() ) {
-            $value = array(
-                OODSP_Settings::DOCSPACE_URL   =>  $docspace_url,
-                OODSP_Settings::DOCSPACE_LOGIN => $docspace_login, 
-                OODSP_Settings::DOCSPACE_PASS  => $docspace_pass,
-                OODSP_Settings::DOCSPACE_TOKEN  => $res_auth['data'],
-            );
+		if ( ! get_settings_errors() ) {
+			$value = array(
+				OODSP_Settings::DOCSPACE_URL   => $docspace_url,
+				OODSP_Settings::DOCSPACE_LOGIN => $docspace_login,
+				OODSP_Settings::DOCSPACE_PASS  => $docspace_pass,
+				OODSP_Settings::DOCSPACE_TOKEN => $res_auth['data'],
+			);
 
-            update_option( 'onlyoffice_docspace_settings', $value );
+			update_option( 'onlyoffice_docspace_settings', $value );
 
-            add_settings_error( 'general', 'settings_updated', __( 'Settings saved', 'onlyoffice-docspace-plugin' ), 'success' );
+			add_settings_error( 'general', 'settings_updated', __( 'Settings saved', 'onlyoffice-docspace-plugin' ), 'success' );
 
-            $res_create_public_user = $oodsp_request_manager->request_create_public_user( $docspace_url, $res_auth['data'] );
+			$res_create_public_user = $oodsp_request_manager->request_create_public_user( $docspace_url, $res_auth['data'] );
 
-            if ( $res_create_public_user['error'] === OODSP_Request_Manager::ERROR_USER_INVITE) {
-                add_settings_error( 'general', 'settings_updated', __( 'Public DocSpace user was not created! View content will not be available on public pages.', 'onlyoffice-docspace-plugin' ), 'warning' );
-            } else if ( $res_create_public_user['error'] === OODSP_Request_Manager::ERROR_SET_USER_PASS) {
-                add_settings_error( 'general', 'settings_updated', __( 'Public DocSpace user already created, but failed to update authorization.', 'onlyoffice-docspace-plugin' ), 'warning' );
-            } else if ( $res_create_public_user['error'] ) {
-                add_settings_error( 'general', 'settings_updated', __( 'Public DocSpace user was not created. View content will not be available on public pages.', 'onlyoffice-docspace-plugin' ), 'warning' );
-            } else {
-                add_settings_error( 'general', 'settings_updated', __( 'Public DocSpace user successfully created.', 'onlyoffice-docspace-plugin' ), 'success' );
-            }
+			if ( OODSP_Request_Manager::ERROR_USER_INVITE === $res_create_public_user['error'] ) {
+				add_settings_error( 'general', 'settings_updated', __( 'Public DocSpace user was not created! View content will not be available on public pages.', 'onlyoffice-docspace-plugin' ), 'warning' );
+			} elseif ( OODSP_Request_Manager::ERROR_SET_USER_PASS === $res_create_public_user['error'] ) {
+				add_settings_error( 'general', 'settings_updated', __( 'Public DocSpace user already created, but failed to update authorization.', 'onlyoffice-docspace-plugin' ), 'warning' );
+			} elseif ( $res_create_public_user['error'] ) {
+				add_settings_error( 'general', 'settings_updated', __( 'Public DocSpace user was not created. View content will not be available on public pages.', 'onlyoffice-docspace-plugin' ), 'warning' );
+			} else {
+				add_settings_error( 'general', 'settings_updated', __( 'Public DocSpace user successfully created.', 'onlyoffice-docspace-plugin' ), 'success' );
+			}
 
-            //Try create current user in DocSpace
-            $user = wp_get_current_user();
+			$user = wp_get_current_user(); // Try create current user in DocSpace.
 
-            $res_docspace_user = $oodsp_request_manager->request_docspace_user( $docspace_url, $user->user_email, $res_auth['data'] );
+			$res_docspace_user = $oodsp_request_manager->request_docspace_user( $docspace_url, $user->user_email, $res_auth['data'] );
 
-            if ($res_docspace_user['error']) {
-                $res_invite_user = $oodsp_request_manager->request_invite_user( 
-                    $user->user_email,
-                    $hash_current_user,
-                    $user->first_name,
-                    $user->last_name,
-                    1, //Room Admin
-                    $res_auth['data']
-                );
+			if ( $res_docspace_user['error'] ) {
+				$res_invite_user = $oodsp_request_manager->request_invite_user(
+					$user->user_email,
+					$hash_current_user,
+					$user->first_name,
+					$user->last_name,
+					1, // Room Admin.
+					$res_auth['data']
+				);
 
-                if ($res_invite_user['error']) {
-                    add_settings_error( 'general', 'settings_updated', sprintf( __( 'Error create user %s in DocSpace!', 'onlyoffice-docspace-plugin' ), $user->user_email), 'error' );
-                } else {
-                    $oodsp_security_manager = new OODSP_Security_Manager();
-                    $oodsp_security_manager->set_oodsp_user_pass( $user->ID, $hash_current_user );
+				if ( $res_invite_user['error'] ) {
+					add_settings_error(
+						'general',
+						'settings_updated',
+						sprintf(
+							/* translators: %s: User email address. */
+							__( 'Error create user %s in DocSpace!', 'onlyoffice-docspace-plugin' ),
+							$user->user_email
+						),
+						'error'
+					);
+				} else {
+					$oodsp_security_manager = new OODSP_Security_Manager();
+					$oodsp_security_manager->set_oodsp_user_pass( $user->ID, $hash_current_user );
 
-                    add_settings_error( 'general', 'settings_updated', sprintf( __( 'User %s successfully created in DocSpace with role Room Admin.', 'onlyoffice-docspace-plugin' ), $user->user_email), 'success' );
-                }
-            } else {
-                add_settings_error( 'general', 'settings_updated', sprintf( __( 'User %s already exits in DocSpace!', 'onlyoffice-docspace-plugin' ), $user->user_email), 'warning' );
-            }
-        }
+					add_settings_error(
+						'general',
+						'settings_updated',
+						sprintf(
+							/* translators: %s: User email address. */
+							__( 'User %s successfully created in DocSpace with role Room Admin.', 'onlyoffice-docspace-plugin' ),
+							$user->user_email
+						),
+						'success'
+					);
+				}
+			} else {
+				add_settings_error(
+					'general',
+					'settings_updated',
+					sprintf(
+						/* translators: %s: User email address. */
+						__( 'User %s already exits in DocSpace!', 'onlyoffice-docspace-plugin' ),
+						$user->user_email
+					),
+					'warning'
+				);
+			}
+		}
 
-        set_transient( 'settings_errors', get_settings_errors(), 30 );
+		set_transient( 'settings_errors', get_settings_errors(), 30 );
 
-        wp_safe_redirect( admin_url( 'admin.php?page=onlyoffice-docspace-settings&settings-updated=true' ) );
-        exit;
-    } else {
-        wp_die( 'The required parameters is missing!', '', array( 'response' => 400 ) );
-    }
+		wp_safe_redirect( admin_url( 'admin.php?page=onlyoffice-docspace-settings&settings-updated=true' ) );
+		exit;
+	} else {
+		wp_die( 'The required parameters is missing!', '', array( 'response' => 400 ) );
+	}
 }
 
-function prepare_value ( $value ) {
-    if ( ! is_array( $value ) ) {
-        $value = trim( $value );
-    }
+/**
+ * Prepare value for saving.
+ *
+ * @param mixed $value     The value.
+ */
+function prepare_value( $value ) {
+	if ( ! is_array( $value ) ) {
+		$value = trim( $value );
+	}
 
-    return wp_unslash( $value );
+	return $value;
 }
