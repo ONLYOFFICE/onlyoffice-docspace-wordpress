73680dd063725cbc0cf8d3e1bc34cc6873a6443d
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2023-05-26 14:19:06 +0300

    fix style for docspace-icon

--- onlyoffice-docspace-wordpress-block/build/index.asset.php
+++ onlyoffice-docspace-wordpress-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b14f985531754b910518');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '867917317b7455e99a99');
