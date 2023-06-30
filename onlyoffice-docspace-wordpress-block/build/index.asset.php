6ec6fabbd494df7b6728e03f1a9e532179ceff8a
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2023-06-30 11:21:54 +0300

    update: build onlyoffice-docspace-wordpress-block

--- onlyoffice-docspace-wordpress-block/build/index.asset.php
+++ onlyoffice-docspace-wordpress-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '771bcd653065d2da2904');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'f0e91daed0ba912cd75e');
