5249a794aaa4f4603246c09c2debb811c8c2ff93
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2023-08-24 10:14:23 +0300

    fixed: replace file or room in component

--- onlyoffice-docspace-wordpress-block/build/index.asset.php
+++ onlyoffice-docspace-wordpress-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'd91194ff504cc6248b7e');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '502f4d7286d1e6134f02');
