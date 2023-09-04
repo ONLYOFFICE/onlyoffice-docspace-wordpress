1f80ca5becbcfbd567e51b3c2b24ffda5c94cce7
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2023-09-04 16:24:27 +0300

    updated: dependencies for onlyoffice-docspace-wordpress-block

--- onlyoffice-docspace-wordpress-block/build/index.asset.php
+++ onlyoffice-docspace-wordpress-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '502f4d7286d1e6134f02');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'e014efab7e821fd438bb');
